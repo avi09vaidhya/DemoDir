ec03d70049730fc61c5689676451f316e97fd9c3
avi.vaidhya <avivaidhya09@gmail.com>
2025-08-09 10:54:56 +0530

    In these file i have added new files and modified some files

--- Basic.java
+++ Basic.java
@@ -1,5 +1,6 @@
 public class Basic{
   public static void main(String[] args){
-    System.out.println("Hello, Avi! This is your code.");
+    System.out.println("Hello, Rakesh! This is your code.");
+    System.out.println("Welcome to the world of Java programming.");
   }
 }
